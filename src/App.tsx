6ffcaaa9a3b6845ae0f86cf05dad549c36167f23
Mattia <plowsharesm@gmail.com>
2026-03-19 20:49:00 +0100

    JOOOO

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import CounterComp from './CounterComp';
+
+function App() {
+
+  return (
+    <>
+      <h1>First Comp</h1>
+
+      <CounterComp/>
+    </>
+  )
+}
+
+export default App
