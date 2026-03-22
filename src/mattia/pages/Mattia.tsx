076e3561aab35186de11d62cda6ffc1438d0cfb8
Mattia <plowsharesm@gmail.com>
2026-03-22 16:19:13 +0100

    chore: Ornderstruktur angepasst

---
+++ src/mattia/pages/Mattia.tsx
@@ -0,0 +1,13 @@
+import CounterComp from "../components/CounterComp"
+import SteamDisplay from "../components/SteamDisplay"
+
+export default function Mario() {
+
+  return (
+    <>
+    <p>Mattia test</p>
+    <SteamDisplay/>
+    <CounterComp/>
+    </>
+  )
+}
