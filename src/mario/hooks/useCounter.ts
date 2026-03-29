2562d9e4b4d001bf786a45cb3720ab07f83fd3a3
Mario <Mayo110uebrigens@gmail.com>
2026-03-29 22:34:11 +0200

    feat: hook2

--- src/mario/hooks/useCounter.ts
+++ src/mario/hooks/useCounter.ts
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const useCounter = () => {
     const [count, setCount] = useState(0);
-
+    
+    useEffect(() => {
+     console.log("Da ist was passiert")
+    }, [count]);
+    
     const increment = () => {setCount(count+1)};
     const reset = () => {setCount(0)};
 
--- src/mario/pages/Mario.tsx
+++ src/mario/pages/Mario.tsx
@@ -1,12 +1,8 @@
-import {useEffect} from "react";
 import CountButton from "../components/Button.tsx";
 import useCounter from "../hooks/useCounter.ts";
 
 const Mario = () => {
   const { count, increment, reset } = useCounter();
-  useEffect(() => {
-    console.log("Da ist was passiert")
-  }, [count]);
   return(
   <div>
     <h1>{count}</h1>
