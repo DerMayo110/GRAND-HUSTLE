5b48dc8169e4b8745de11552160a5bc3c8b8e375
Mario <Mayo110uebrigens@gmail.com>
2026-03-29 21:33:59 +0200

    feat: jooo2

--- src/mario/components/Button.tsx
+++ src/mario/components/Button.tsx
@@ -1,10 +1,16 @@
+ import styles from "./Button.module.css";
  type ButtonProps = {
     text: string;
     onClick: () => void;
   }
 const CountButton = (props: ButtonProps) => {
       return(
-          <button onClick={props.onClick}>{props.text}</button>
+        <button 
+            className={styles.button}
+            onClick={props.onClick}
+            >
+            {props.text}
+        </button>
       )
   }
 export default CountButton;
--- src/mario/pages/Mario.tsx
+++ src/mario/pages/Mario.tsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
 import CountButton from "../components/Button.tsx";
+
 const Mario = () => {
   const [count, setCount] = useState(0);
   useEffect(() => {
