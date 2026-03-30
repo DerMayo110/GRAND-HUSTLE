46cd637fe65b486b59ceb112d5019ee6bf44b6fa
Mario <Mayo110uebrigens@gmail.com>
2026-03-30 22:04:06 +0200

    feat: Lists and Keys

--- src/mario/pages/Mario.tsx
+++ src/mario/pages/Mario.tsx
@@ -1,11 +1,17 @@
 import Button from "../components/Button.tsx";
 import useCounter from "../hooks/useCounter.ts";
 import useToggle from "../hooks/useToggle.ts";
+import DasLager from "../components/DasLager.tsx";
 
 
 const Mario = () => {
   const { count, increment, reset } = useCounter();
   const {visible,toggle} = useToggle();
+  const items = [
+    { id: 1, name: 'Stein', quantity: 3 },
+    { id: 2, name: 'Holz', quantity: 1 },
+    { id: 3, name: 'Hacke', quantity: 99 },
+  ];
   
   return(
   <div>
@@ -14,6 +20,7 @@ const Mario = () => {
     <Button text="Zähl hoch" onClick={increment} />
     <Button text="Reset" onClick={reset} />
     <Button text="Text ein-/ausblenden" onClick={toggle} />
+    <DasLager items={items} />
   </div>
   
   )
